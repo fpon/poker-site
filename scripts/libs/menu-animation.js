602ecdb26775d3e8cac0c32f78d7a49a72764e68
fpon <79495350+fpon@users.noreply.github.com>
2022-08-06 00:49:06 +0900

    menu.jsの変更３

--- scripts/libs/menu-animation.js
+++ scripts/libs/menu-animation.js
@@ -4,19 +4,20 @@ window.addEventListener('DOMContentLoaded', (e) =>{
     FadeUp.classList.add( "fade-up" );
 });
 
-const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-for (let i = 0; i < smoothScrollTrigger.length; i++){
-  const clickEventType = (( window.ontouchstart!==null ) ? 'click':'touchend');
-  smoothScrollTrigger[i].addEventListener(clickEventType, (e) => {
-    let href = smoothScrollTrigger[i].getAttribute('href');
-    let targetElement = document.getElementById(href.replace('#', ''));
-    const rect = targetElement.getBoundingClientRect().top;
-    const target = rect + window.pageYOffset - 55;
-    window.scrollTo({
-        top: target,
-      });
-  });
-}
+// const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+// for (let i = 0; i < smoothScrollTrigger.length; i++){
+//   const clickEventType = (( window.ontouchstart!==null ) ? 'click':'touchend');
+//   smoothScrollTrigger[i].addEventListener(clickEventType, (e) => {
+//     e.preventDefault();  
+//     let href = smoothScrollTrigger[i].getAttribute('href');
+//     let targetElement = document.getElementById(href.replace('#', ''));
+//     const rect = targetElement.getBoundingClientRect().top;
+//     const target = rect + window.pageYOffset - 55;
+//     window.scrollTo({
+//         top: target,
+//       });
+//   });
+// }
 
 class MobileMenu {
     constructor() {
--- scripts/libs/privacy-animation.js
+++ scripts/libs/privacy-animation.js
@@ -12,6 +12,9 @@ for (let i = 0; i < smoothScrollTrigger.length; i++){
     let targetElement = document.getElementById(href.replace('#', ''));
     const rect = targetElement.getBoundingClientRect().top;
     const target = rect + window.pageYOffset - 55;
+    window.scrollTo({
+        top: target,
+      });
   });
 }
 
