95aa587384233ccc9117e9a7b6cf0789ff7d4147
fpon <79495350+fpon@users.noreply.github.com>
2022-08-06 03:35:39 +0900

    menu.jsの編集前最終

--- scripts/libs/mobile-menu.js
+++ scripts/libs/mobile-menu.js
@@ -1,19 +1,3 @@
-const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-for (let i = 0; i < smoothScrollTrigger.length; i++){
-  const clickEventType = (( window.ontouchstart!==null ) ? 'click':'touchend');
-  smoothScrollTrigger[i].addEventListener(clickEventType, (e) => {
-    e.preventDefault();
-    let href = smoothScrollTrigger[i].getAttribute('href');
-    let targetElement = document.getElementById(href.replace('#', ''));
-    const rect = targetElement.getBoundingClientRect().top;
-    const target = rect + window.pageYOffset - 55;
-    window.scrollTo({
-      top: target,
-      behavior: 'smooth',
-    });
-  });
-}
-
 class MobileMenu {
   constructor() {
     this.DOM = {}; 
--- scripts/libs/privacy-animation.js
+++ scripts/libs/privacy-animation.js
@@ -4,17 +4,21 @@ window.addEventListener('DOMContentLoaded', (e) =>{
     FadeUp.classList.add( "fade-up" );
 });
 
+
+
 const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
 for (let i = 0; i < smoothScrollTrigger.length; i++){
   const clickEventType = (( window.ontouchstart!==null ) ? 'click':'touchend');
   smoothScrollTrigger[i].addEventListener(clickEventType, (e) => {
+    e.preventDefault();
     let href = smoothScrollTrigger[i].getAttribute('href');
     let targetElement = document.getElementById(href.replace('#', ''));
     const rect = targetElement.getBoundingClientRect().top;
     const target = rect + window.pageYOffset - 55;
     window.scrollTo({
-        top: target,
-      });
+      top: target,
+      behavior: 'smooth',
+    });
   });
 }
 
