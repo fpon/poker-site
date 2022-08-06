aef89308acc47e18ce98a7c58372f896906a2de6
fpon <79495350+fpon@users.noreply.github.com>
2022-08-06 14:59:00 +0900

    スマホ画面完成（仮）

--- scripts/libs/privacy-animation.js
+++ scripts/libs/privacy-animation.js
@@ -8,6 +8,7 @@ class MobileMenu {
     constructor() {
       this.DOM = {}; 
       this.DOM.btn = document.querySelector('.mobile-menu-btn');
+      this.DOM.cover = document.querySelector('.mobile-nav__list');
       this.DOM.container = document.querySelector('#global-container');
       this.eventType = this._getEventType();
       this._addEvent();
@@ -29,6 +30,7 @@ class MobileMenu {
   
     _addEvent() {
       this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+      this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
     }
   }
   
